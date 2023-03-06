40e62df8b1415b33e4290dd7b593e0074f56e25c
hahy23 <hahy23@naver.com>
2023-03-06 22:10:15 +0900

    커밋

--- 07_java_object_arry/src/step7_01/objectArray/AlonePractice.java
+++ 07_java_object_arry/src/step7_01/objectArray/AlonePractice.java
@@ -1,6 +1,12 @@
 package step7_01.objectArray;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 class StudentDTO2{
@@ -18,6 +24,16 @@ class StudentController2{
 	ArrayList<StudentDTO2> studentList1 = new ArrayList<StudentDTO2>();
 	
 	
+	
+	void printStudent() {
+		for (int i = 0; i < studentList1.size(); i++) {
+			System.out.println("id : " + studentList1.get(i).id + " pw : " + studentList1.get(i).pw);
+			
+			
+			
+		}
+	}
+	
 	int checkId(String id) {
 		int checkId = -1;
 		
@@ -34,6 +50,79 @@ class StudentController2{
 	
 	void addStudent(StudentDTO2 studentDTO2) {
 		studentList1.add(studentDTO2);
+	}
+	
+	void loadStudent(ArrayList<StudentDTO2> studentList) {
+		this.studentList1 = studentList;
+	}
+	
+	
+	
+	int removeStudent(StudentDTO2 st) {
+		int check = -1;
+		
+		for (int i = 0; i < studentList1.size(); i++) {
+			if(st.id.equals(studentList1.get(i).id) && st.pw.equals(studentList1.get(i).pw)){
+				check = i;
+			}
+		}
+		
+		
+		
+		
+		return check;
+		
+	}
+	
+	String outData() {
+		String data = "";
+		int count = studentList1.size();
+		if(count ==0) {
+			return data;
+		}
+		data += count;
+		data +="\n";
+		for (int i = 0; i < count; i++) {
+			data += studentList1.get(i).id;
+			data += ",";
+			data +=studentList1.get(i).pw;
+			if(count-1 != i) {
+				data += "\n";
+				
+			}
+		}
+		return data;
+	}
+	
+	
+	
+	
+	StudentDTO2 perfectDelate(int checkId) {
+		StudentDTO2 temp = studentList1.get(checkId);
+		
+		studentList1.remove(checkId);
+		
+		return temp;
+		
+		
+	}
+	
+	int getSize() {
+		return studentList1.size();
+	}
+	
+	void sortData() {
+		for (int i = 0; i < studentList1.size(); i++) {
+			for (int j = 0; j < studentList1.size(); j++) {
+				if(studentList1.get(i).id.compareTo(studentList1.get(j).id) < 0) {
+					StudentDTO2 temp = studentList1.get(i);
+					studentList1.set(i, studentList1.get(j));
+					studentList1.set(j, temp);
+					
+					
+				}
+			}
+		}
 	}
 	
 }
@@ -86,9 +175,117 @@ public class AlonePractice {
 			}
 			
 			else if(sel == 2) {
+				controller.printStudent();
+				
+				StudentDTO2 temp = new StudentDTO2();
+				System.out.println("[탈퇴] id를 입력해주세요 :");
+				temp.id = scan.next();
+				System.out.println("[탈퇴] pw를 입력해주세요 : ");
+				temp.pw = scan.next();
+				
+				int checkId = controller.removeStudent(temp);
+				
+				if(checkId == -1) {
+					System.out.println("[메세지] 없는 아이디입니다.");
+					
+				}
+				else {
+					StudentDTO2 tmp = controller.perfectDelate(checkId);
+					System.out.println(tmp.id +"님 탈퇴 되었습니다.");
+					
+				}
 				
 				
 				
+				
+			}
+			
+			else if(sel == 3) {
+				controller.sortData();
+				controller.printStudent();
+			}
+			
+			else if(sel == 4) {
+				controller.printStudent();
+			}
+			
+			else if(sel == 5) {
+				FileWriter fw = null;
+				if(controller.getSize() == 0) {
+					continue;
+				}
+				else {
+					try {
+						fw = new FileWriter(fileName);
+						int studentListCount = controller.studentList1.size();
+						String data = controller.outData();
+						
+						if(!data.equals("")) {
+							fw.write(data);
+						}
+						
+					}catch(Exception e) {
+						e.printStackTrace();
+					}finally {
+						try {fw.close();}catch(IOException e) { e.printStackTrace();}
+					}
+						
+						
+				}
+					
+				
+				
+				
+				
+				
+			}
+			
+			
+			else if(sel == 6) {
+				FileReader fr = null;
+				BufferedReader br = null;
+				
+				try {
+					
+					File file = new File(fileName);
+					
+					if(file.exists()) {
+						fr = new FileReader(file);
+						br = new BufferedReader(fr);
+						ArrayList<StudentDTO2> studentList = new ArrayList<StudentDTO2>();
+						
+						
+						String line = br.readLine();
+						int count = Integer.parseInt(line);
+						
+						for (int i = 0; i < count; i++) {
+							StudentDTO2 temp = new StudentDTO2();
+							line = br.readLine();
+							String[] value = line.split(",");
+							temp.id = value[0];
+							temp.pw = value[1];
+							studentList.add(temp);
+						}
+						
+						
+						controller.loadStudent(studentList);
+						
+						
+						
+					}
+					
+					controller.printStudent();
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+				}finally {try { br.close();}catch(IOException e) {e.printStackTrace();}
+						  try { fr.close();}catch(IOException e) {e.printStackTrace();}
+				}
+			}
+			
+			else if(sel == 7) {
+				System.out.println("프로그램을 종료하겠습니다.");
+				break;
 			}
 		}
 	}
